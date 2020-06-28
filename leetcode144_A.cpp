888ea8b060cde131f48c5e916b8a879ed3f47208
yufeng8607 <46846162+yufeng8607@users.noreply.github.com>
2020-06-28 18:39:48 +0800

    二叉树先序遍历递归形式

---
+++ leetcode144_A.cpp
@@ -0,0 +1,25 @@
+//递归形式
+void preV(TreeNode* root, vector<int>& res);
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        preV(root, res);
+        return res;
+    }
+};
+
+void preV(TreeNode* root, vector<int>& res)
+{
+    if(root == nullptr)
+    {
+        return;
+    }
+    else
+    {
+        res.push_back(root->val);
+    }
+    preV(root->left, res);
+    preV(root->right, res);
+}
